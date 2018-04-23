7a55af2867489bfe2715ef84c5dbb3cf4925e22a
Charles Wong <charleswong95@gmail.com>
2018-04-23 17:54:47 -0400

    Added script to create tfrecord files

---
+++ image_utils.py
@@ -0,0 +1,12 @@
+import cv2
+
+def load_image(addr):
+    # read an image and resize to (224, 224)
+    # cv2 load images as BGR, convert it to RGB
+    img = cv2.imread(addr)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32)
+    return img
+
+
